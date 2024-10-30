43bfeb545e8052808efa30388db5056671034b9f
anobee <HP@DESKTOP-KFAVCD8>
2024-10-30 10:58:16 +0700

    end

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
-class Persegipanjang:
-    def _init_(self, panjang, lebar):
+class PersegiPanjang:
+    def __init__(self, panjang, lebar):  
         self.panjang = panjang
         self.lebar = lebar
 
@@ -9,23 +9,23 @@ class Persegipanjang:
     def hitung_luas(self):
         return self.panjang * self.lebar
 
-    def _str_(self):
+    def __str__(self):  
         return f"Persegi Panjang, panjang {self.panjang} cm, dan lebar {self.lebar} cm"
 
-if _name_ == "_main_":
+if __name__ == "__main__":  
     try:
+       
         panjang = float(input("Masukkan panjang persegi panjang (cm): "))
         lebar = float(input("Masukkan lebar persegi panjang (cm): "))
         
+    
         if panjang <= 0 or lebar <= 0:
             print("Error: Panjang dan lebar harus lebih besar dari 0.")
         else:
-            persegi_panjang = Persegipanjang(panjang, lebar)
+            persegi_panjang = PersegiPanjang(panjang, lebar)
             print(persegi_panjang)
             print("Keliling:", persegi_panjang.hitung_keliling(), "cm")
             print("Luas:", persegi_panjang.hitung_luas(), "cm^2")
     
     except ValueError:
         print("Error: Input harus berupa angka.")
-        
-    #seles
